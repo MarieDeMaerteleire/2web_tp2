5223280e1361ac7036569ce3546a042ae90aa6b0
Marie De Maerteleire <Marie De Maerteleire>
2017-03-01 15:28:56 +0100

    First Commit with php with Maxime Senecal best teacher ever after Sir BOUTHORS

---
+++ index.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: riima
+ * Date: 01/03/2017
+ * Time: 15:07
+ */
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>We ♥ Unicorns</title>
+    <link rel="stylesheet" type="text/css" href="css/main.css">
+</head>
+<body>
+    <header id="head">
+        <h1 class="mainTitle"> We love Unicorns</h1>
+        <ul id="menu">
+            <li class="first">
+                <a href="#reality">Real Unicorns</a>
+            </li>
+            <li>
+                <a href="#food">What do they eat?</a>
+            </li>
+            <li>
+                <a href="#find">How do I find them ?</a>
+            </li>
+        </ul>
+    </header>
+    <div id="reality">
+        <h2>Real Unicorns</h2>
+        <p>Unicorns are Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla ut elementum tortor, id scelerisque tellus. Fusce turpis dolor, lacinia convallis dictum id, pretium vitae nisl. Vivamus iaculis dui at nisl aliquet molestie. Proin sit amet posuere enim. Nunc venenatis efficitur sem eu egestas. Curabitur eget efficitur est. Cras tincidunt in justo ac maximus. Praesent eget finibus nulla, ac cursus metus.
+
+Curabitur laoreet vel orci in mollis. Vivamus id auctor dui. Maecenas sit amet gravida lectus, eu gravida metus. In sollicitudin dui a ex sodales sollicitudin. Nunc sollicitudin eu orci a posuere. Duis varius tempus nibh vitae pellentesque. Fusce posuere finibus elit vitae varius. Sed et augue porttitor, pulvinar nisi at, condimentum mauris. Sed lorem felis, suscipit sit amet odio non, maximus varius nunc. Morbi dignissim, enim a pharetra cursus, ante dui tristique tellus, eget rhoncus quam magna eget magna. Curabitur vitae mauris quis tortor imperdiet imperdiet porttitor ut velit. Cras commodo condimentum pulvinar. Nunc eleifend ipsum at mi ullamcorper varius.
+
+Fusce tempor leo sit amet massa maximus, quis pulvinar tellus commodo. Cras eget porta urna, vitae commodo purus. Quisque molestie accumsan posuere. Suspendisse potenti. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Duis in urna eu libero bibendum laoreet. Sed eget risus elementum, euismod sapien sed, ultrices lorem. Nam et justo faucibus, laoreet sapien vitae, ultricies nisl. Mauris finibus mauris libero, at lobortis lorem ornare nec. Nulla sit amet nibh id justo porttitor ultricies. Suspendisse magna felis, elementum vel risus vitae, varius sagittis tortor. Etiam vitae odio eget turpis fringilla pharetra a eget tellus. Vestibulum tempus rhoncus lectus at aliquet. Nunc a aliquam erat. Etiam nibh tortor, finibus non accumsan ut, aliquet eu arcu.
+
+Curabitur consequat viverra sodales. Fusce id turpis enim. Curabitur commodo lacus vel risus pharetra, eu laoreet sem eleifend. Aliquam mollis sollicitudin erat, sit amet tempor dolor dapibus quis. Pellentesque dictum metus tempor, efficitur lectus tincidunt, pellentesque lacus. Integer et elit nec augue feugiat sodales. Phasellus tempor fermentum aliquam.
+
+Morbi leo justo, gravida nec turpis at, rhoncus porta velit. Praesent vel nisi non tortor faucibus vulputate non sit amet velit. Suspendisse tristique dolor elit, vitae ornare eros accumsan at. Phasellus ut volutpat augue. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nunc luctus, nibh at bibendum tempus, metus nunc tempus magna, ac ornare lorem neque vitae nunc.</p>
+    </div>
+    <div id="food">
+        <p>Unicorns like rainbow cakes. It's sweet and good looking ! They love it !
+        Majestic unicorns eat majestic food !</p>
+        <img src="img/unicorn.jpg" alt="unicorns cake rainbow" id="unicornImage"/>
+    </div>
+    <div id="find">
+
+    </div>
+</body>
+</html>
